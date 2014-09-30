fa2382c7960b9f3d59b978c80f1b8f51b139e30e
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2014-09-30 11:49:39 -0400

    Cleaned up pngquant.js
    Removed a ton of repetition and unnecessary specificity.

--- _scripts/pngquant.js
+++ _scripts/pngquant.js
@@ -54,86 +54,28 @@ $( document ).ready( function(){
                     console.log("cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " not checked");
                 }
 
-        //RANGE, FILE, DROPDOWN, TEXTBOX
+        //ANYTHING OTHER THAN A CHECKBOX
 
             //Check if element contains prefix AND suffix
-            } else if
-                (
-                    $(cmdArgs[index]).is('input[type="range"]') && argPrefix && argSuffix ||
-                    $(cmdArgs[index]).is('input[type="file"]') && argPrefix && argSuffix ||
-                    $(cmdArgs[index]).is('select') && argPrefix && argSuffix ||
-                    $(cmdArgs[index]).is('textarea') && argPrefix && argSuffix
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch40 = "-m 'message'"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = argPrefix + $(cmdArgs[index]).val() + argSuffix;
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " has prefix and suffix" );
+            } else if ( argPrefix && argSuffix ) {
+                //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch40 = "-m 'message'"
+                window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = argPrefix + $(cmdArgs[index]).val() + argSuffix;
+                console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " has prefix and suffix" );
             //Check if element contains just a prefix
-            } else if
-                (
-                    $(cmdArgs[index]).is('input[type="range"]') && argPrefix ||
-                    $(cmdArgs[index]).is('input[type="file"]') && argPrefix ||
-                    $(cmdArgs[index]).is('select') && argPrefix ||
-                    $(cmdArgs[index]).is('textarea') && argPrefix
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch4 = "--speed 1"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = argPrefix + $(cmdArgs[index]).val();
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " has prefix only" );
+            } else if ( argPrefix ) {
+                //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch4 = "--speed 1"
+                window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = argPrefix + $(cmdArgs[index]).val();
+                console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " has prefix only" );
             //Check if element contains just a suffix
-            } else if
-                (
-                    $(cmdArgs[index]).is('input[type="range"]') && argSuffix ||
-                    $(cmdArgs[index]).is('input[type="file"]') && argSuffix ||
-                    $(cmdArgs[index]).is('select') && argSuffix ||
-                    $(cmdArgs[index]).is('textarea') && argSuffix
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch38 = "12 px"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = $(cmdArgs[index]).val() + argSuffix;
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " has suffix only" );
+            } else if ( argSuffix ) {
+                //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch38 = "12 px"
+                window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = $(cmdArgs[index]).val() + argSuffix;
+                console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " has suffix only" );
             //Check if it doesn't contain a prefix OR a suffix
-            } else if
-                (
-                    $(cmdArgs[index]).is('input[type="file"]') ||
-                    $(cmdArgs[index]).is('input[type="range"]') ||
-                    $(cmdArgs[index]).is('select') ||
-                    $(cmdArgs[index]).is('textarea')
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, cmdSwitch12 = "--silent"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = $(cmdArgs[index]).val();
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " no prefix or suffix" );
-
-
-        //If the data-argOrder is used on an element that has not been specified above, attempt to grab a value
-
-            //Check if element contains prefix AND a suffix
-            } else if
-                (
-                    argPrefix && argSuffix
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch40 = "-m 'message'"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = argPrefix + $(cmdArgs[index]).val() + argSuffix;
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " unknown type, has prefix and suffix" );
-            //Check if element contains just a prefix
-            } else if
-                (
-                    argPrefix
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch4 = "--speed 1"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = argPrefix + $(cmdArgs[index]).val();
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " unknown type, has prefix only" );
-            //Check if element contains just a suffix
-            } else if
-                (
-                    argSuffix
-                ) {
-                    //creates a variable named cmdSwitch# and sets it to the switch value, so cmdSwitch38 = "12 px"
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = $(cmdArgs[index]).val() + argSuffix;
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " unknown type, has suffix only" );
-            //Check if it doesn't contain a prefix OR a suffix
-            } else {
-                    //creates a variable named cmdSwitch# and attempts to get the value of the unknown element
-                    window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = $(cmdArgs[index]).val();
-                    console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " unknown type, no prefix or suffix" );
-            }
+            } else if {
+                //creates a variable named cmdSwitch# and sets it to the switch value, cmdSwitch12 = "--silent"
+                window["cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder")] = $(cmdArgs[index]).val();
+                console.log( "cmdSwitch" + $(cmdArgs[index]).attr("data-argOrder") + " no prefix or suffix" );
         };
 
         //1. Create an array of all cmdSwitch#'s        [cmdSwitch99, cmdSwitch21, cmdSwitch1, cmdSwitch2, cmdSwitch3, cmdSwitch4, cmdSwitch40]
